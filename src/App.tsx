8c8b73feda1149544e0527c859fc4ea40fd8fb66
mzw <zhenwei.ma@duijiaba.com>
2021-04-30 19:04:24 +0800

    doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
-import styles from './App12.css'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
